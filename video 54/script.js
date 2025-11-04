fdf1bad91272f8cc9ce04a0e9d69115aa74903e2
coderninjai <pradipgajghate1@gmail.com>
2025-11-04 18:33:31 +0530

    Intro to JS

---
+++ video 54/script.js
@@ -0,0 +1,19 @@
+
+alert("hello world");
+console.log("Hey I am console");
+console.log("nna");
+
+
+var a= prompt("Enter your number");
+var IsTrue=confirm("Are you sure you want to burn")
+
+if(IsTrue){
+    console.log("you are burning ");
+}else{
+    console.log("computer is not blasting");
+}
+console.log(a)
+
+document.title="hey "
+
+document.body.style.backgroundColor="red"
