e6f789386676433e829513603778457fb5932557
coderninjai <pradipgajghate1@gmail.com>
2025-11-05 18:41:39 +0530

    Strings

---
+++ video 60/index.js
@@ -0,0 +1,24 @@
+console.log("Strings are immutable")
+let a ="Pradip";
+
+
+console.log(a[0]);
+
+console.log(a.length);
+
+let real_name ="Pradip";
+let friend="friend";
+
+console.log("His name is "+ real_name+ " a nd his friends name is "+friend);
+
+real_name=`rohh"han`
+
+let b = "shivam"
+console.log(b.toUpperCase())
+
+console.log(b.slice(1,4))
+
+console.log(b.replace("sh","77"))
+
+console.log(b.concat(a,"Aishw"))
+
