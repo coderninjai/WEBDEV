138df0beb2d1ffb85b438c6bf8e9bf857dded19e
coderninjai <pradipgajghate1@gmail.com>
2025-11-05 10:20:55 +0530

    Javascript Var, let, data types

---
+++ video 55/script.js
@@ -0,0 +1,38 @@
+// var a =5;
+let a =4;
+var b =6;
+var c= "Harry";
+
+console.log(a+b+8);
+console.log(typeof a)
+
+
+console.log("javascript is case sensitive");
+
+const a1=6;
+// a1=a1+1;   not allowed 
+
+// Use let for var
+
+
+
+let x="Harry bhai";
+let y=22;
+let z= 3.55;
+const p =true;
+let q=undefined;
+let r =null;
+
+console.log(x,y,z,p,q,r);
+
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+
+let o={
+    "name":"harry",
+    "job role":555,
+    "is_handsome":true
+}
+
+console.log(o)
+o.salary ="500crores";
+console.log(o)
