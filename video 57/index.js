de43e6ab9c7dac4d84cf8857662afd8897440b08
coderninjai <pradipgajghate1@gmail.com>
2025-11-05 13:15:04 +0530

    Javascript loops

---
+++ video 57/index.js
@@ -0,0 +1,35 @@
+console.log("I am tutorial on loop");
+
+let a =1;
+
+// for(let i=0;i<100;i++){
+//     console.log(a+i);
+// }              
+
+// let obj ={
+//     name:"harry",
+//     role:"programmer",
+//     company:"CodeWithHarry AI"
+// }
+
+// for(const key in obj){
+//     const element =obj[key];
+//     console.log(key, element);
+// }
+
+// for(const key of "harry"){
+//     console.log(key);
+// }
+
+
+i=5;
+while(i<6){
+    console.log(i);
+    i++;
+}
+
+i=0;
+do{
+    console.log(i);
+    i++;
+}while(i<6);
