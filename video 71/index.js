73099081d3cd79ddfb8d6f530a722ac62a49cc8d
coderninjai <pradipgajghate1@gmail.com>
2025-11-08 18:11:52 +0530

    Inserting and removing Elements using Javascript

---
+++ video 71/index.js
@@ -0,0 +1,19 @@
+document.querySelector(".container").innerText
+console.log(
+    document.querySelector(".container").outerHTML);
+document.querySelector(".box").getAttribute("style")
+document.querySelector(".box").setAttribute("style", "display:inline")
+document.querySelector(".box").attributes
+document.querySelector(".box").removeAttribute("style")
+
+document.designMode.on
+
+let cont = document.querySelector(".container")
+cont.insertAdjacentHTML("afterend", "<b> I am optimus prime and i call all the descendents ")
+
+
+
+document.querySelector(".container").classList
+document.querySelector(".container").classList.add("me")
+document.querySelector(".container").classList.remove("me")
+// document.querySelector(".container").classList.toggle("me")  alternate on to off to on
