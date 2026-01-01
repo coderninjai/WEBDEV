32219d6e61350c0467ad5ff4f503d3b7200aaa90
coderninjai <pradipgajghate1@gmail.com>
2026-01-01 14:36:44 +0530

    conditional rendering

---
+++ video 110/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [showbtn, setshowbtn] = useState(true)
+
+  const Todo = ({todo}) =>  {
+    return(<>
+      <div className="todo">I am todo</div></>)
+  }
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+     
+      <h1>Vite + React</h1>
+      {showbtn ? <button>I will be button</button> : "noob"}
+      <div className="card">
+        <button onClick={() => setshowbtn(!showbtn)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+       <Todo/>
+    </>
+  )
+}
+
+export default App
